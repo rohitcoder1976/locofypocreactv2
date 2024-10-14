31043c0e5a3c654e0f85c72c30666b78650f0e9a
rohitcoder1976 <rmarath2008@gmail.com>
2024-10-15 04:48:23 +0530

    Locofy import

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { Button } from './components/ui/button'
+import { Checkbox } from './components/ui/checkbox'
+import { SearchInput } from './components/ui/searchInput'
+
+function App() {
+
+  return (
+    <>
+      <Button>Hello World!</Button>
+      <Checkbox />
+      <SearchInput />
+    </>
+  )
+}
+
+export default App
